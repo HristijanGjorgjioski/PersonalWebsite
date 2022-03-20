391604e357e80da68a74fadc28973a83051b1117
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-03-20 17:44:59 +0100

    Importing sass to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "./index.css";
-
+import "./App.sass";
 import Navbar from "./components/Navbar/Navbar";
-import Background from "./assets/background.jpg";
 
 const App: React.FC = () => {
   return (
-    <div className='text-center'>
-      <img alt='background' className='background z-0' src={Background} />
+    <div className='main-container'>
       <div className='w-10/12 bg-gray-300 z-10 rounded-full'>
         <Navbar />
       </div>
