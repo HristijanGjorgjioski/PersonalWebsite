fc4b65cd43a953d129c6c306087e870bde6faaee
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-03-20 18:01:22 +0100

    Worked on app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.sass";
+import "./App.styles.sass";
 import Navbar from "./components/Navbar/Navbar";
 
 const App: React.FC = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./sass/globalStyles.sass";
+import "./sass/globalStyles.styles.sass";
 import App from "./App";
 
 ReactDOM.render(
